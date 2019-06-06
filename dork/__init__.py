8e584a06bcb473fb185d4ea76af0a100abf96a6d
Casey Jones <cjone183@msudenver.edu>
2019-06-06 10:50:14 -0600

    add dev team to __init__.py

--- dork/__init__.py
+++ dork/__init__.py
@@ -5,4 +5,11 @@
 __version__ = '0.1.0'
 __author__ = ", ".join([
     "Luke Smith",
+    "Casey Jones",
+    "Gregory Dews",
+    "Derek Haviland",
+    "Edwin Hernandez Sanchez",
+    "Matt Hurt",
+    "David Kittleso",
+    "Daniel L'Episcopo"
 ])
