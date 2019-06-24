9bc30d82c092a551963dabc7da21862b4c1a8d90
Daniel LEpiscopo <dannyjlepiscopo@gmail.com>
2019-06-24 11:59:25 -0600

    Monday, June 24th - 11:59AM

--- dork/Premade_Maze.py
+++ dork/Premade_Maze.py
@@ -1,17 +1,18 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import yaml_parser_Maze
 
 G = nx.Graph()
 
-G.add_node(1)
+G.add_node("Empty Room")
 G.add_node(2)
 G.add_node(3)
 G.add_node(4)
 G.add_node(5)
 
-G.add_edge(1, 2)
+G.add_edge("Empty Room", 2)
 G.add_edge(3, 4)
-G.add_edge(4, 1)
+G.add_edge(4, "Empty Room")
 G.add_edge(2, 3)
 G.add_edge(4, 5)
 
