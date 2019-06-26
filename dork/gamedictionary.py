2fb4e077575552467bd6bfa6bedbf16ddf15112a
Greg Dews <gdews@msudenver.edu>
2019-06-26 12:16:35 -0600

    Successful testing process for REPL and actions

--- dork/gamedictionary.py
+++ dork/gamedictionary.py
@@ -2,18 +2,18 @@
 """
 __all__ = ['MOVEMENT', 'ACTION']
 
-MOVEMENT = {'north' : 'You have moved north',
-            'east' : 'You have moved east',
-            'south' : 'You have moved south',
-            'west' : 'You have moved west'
-           }
+MOVEMENT = {'north': 'You have moved north',
+            'east': 'You have moved east',
+            'south': 'You have moved south',
+            'west': 'You have moved west'
+            }
 
-ACTION = {'jump' : 'you have jumped',
-          'run' : 'You have ran',
-          'cry' : 'You started crying'
+ACTION = {'jump': 'you have jumped',
+          'run': 'You have ran',
+          'cry': 'You started crying',
           'movement': MOVEMENT,
           'go': MOVEMENT,
-         }
+          }
 
 if __name__ == "__main__":
     pass
--- tests/test_dork_cli.py
+++ tests/test_dork_cli.py
@@ -8,6 +8,15 @@ from dork import cli
 #     test some known commands
 
 
+def test_evaluate():
+    """ evaluate() is out of date"""
+
+    assert cli.evaluate('jump') == 'you have jumped'
+    assert cli.evaluate('go').get('north') == 'You have moved north'
+    assert cli.evaluate('jump') == 'you have jumped'
+    assert cli.evaluate('jump') == 'you have jumped'
+
+
 def test_repl(mocker):
     """ REPL should loop until user inputs quit"""
     mock_input = mocker.patch('builtins.input')
@@ -15,9 +24,9 @@ def test_repl(mocker):
                               ("quit"),
                               ("nope")]
     cli.repl()
-    assert mock_input.call_counter == 2  # pylint is a bitch
-    
-    
+    assert mock_input.call_count == 2  # pylint is a bitch
+
+
 def test_cli_exists(run):
     '''dork.cli.main should always exist and run.'''
     assert "main" in vars(cli), "Dork.cli should define a main method"
@@ -26,7 +35,7 @@ def test_cli_exists(run):
         run(cli.main)
     except io.UnsupportedOperation:  # expected
         # prompt_toolkit raises when run non-interactively (e.g. in a test)
-        return
+        pass
     except:  # noqa: E722
         raise AssertionError("cannot run 'dork' command")
 
@@ -36,9 +45,3 @@ def test_cli_help(run):
     out, err = run(cli.main, "-h")
     assert "usage: " in out, \
         "Failed to run the cli.main method: {err}".format(err=err)
-
-
-def test_evaluate():
-    """ evaluate() is out of date"""
-
-    assert cli.evaluate('jump') == 'you have jumped'
