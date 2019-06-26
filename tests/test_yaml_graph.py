8ccfab57d45988dc2254088df0c8688fb1ebb348
Daniel LEpiscopo <dannyjlepiscopo@gmail.com>
2019-06-26 02:00:46 -0600

    Wednesday, June 26th @ 2:00AM

--- tests/test_yaml_graph.py
+++ tests/test_yaml_graph.py
@@ -4,15 +4,11 @@ from unittest import mock
 
 # *** Checking File Tests ***
 
-# Test 1 - Bad String/Input
-# Check if valid file path
-
-# Test 2 - Good file, bad text
-# assert error on read
-
-# Test 3 - Good file
+# Test 1 - Good file
 
 # a) Check if a path exists
+
+
 def _read(file_path, f):
     file_path = 'maze.yml'
     with open(file_path) as f:
@@ -20,48 +16,58 @@ def _read(file_path, f):
 
 
 # b) Open the file
- def _good_file(path):
+def _good_file(path):
     if os.path.exists('maze.yaml'):
         return True
-    else: 
-        return False 
+    else:
+        return False
 
 
 # c) First line == "Rooms"
 def _first_line(f):
-        first_line = f.readline()
-        if first_line == "Rooms":
-            return True
-        else:
-            return False
+    first_line = f.readline()
+    if first_line == "Rooms":
+        return True
+    else:
+        return False
+
+
 # d) Second line == "Empty Room"
-def _second_line():
+def _second_line(f):
+        second_line = f.readline()
+        if second_line == "Empty Room":
+                return True
+        else:
+                return False
 
-        
+    # *** Creating Nodes Tests ***
 
-        # *** Creating Nodes Tests ***
+    # Test 1 - Create a No Nodes
+    # Check if nothing exists
 
-        # Test 1 - Create a No Nodes
-        #
+    # Test 2 - Create a Node
+    # G[0] exists
+    # G[0] data
 
-        # Test 2 - Create a Node
-        #
+    # Test 3 - Create second Node
+    # G[1] exists
+    # G[1] data
 
-        # Test 3 - Create Two Nodes
-        #
+    # Test 4 - Create Two Nodes
+    # G[2] does not exist
 
-        # Test 4 - Create Node Automatically
-        # Node exists
-        # Node Based on a File Data
+    # Test 4 - Create Node Automatically
+    # Node exists
+    # Node Based on a File Data
 
-        # Test 5 - Create Multiple Node Automatically
-        # Last Node == lastFileData
+    # Test 5 - Create Multiple Node Automatically
+    # Last Node == lastFileData
 
-    def runtests(file_path, f):
+    def runtests():
         _read('file_path', 'f')
-       # _good_file(path)
-       # _first_line(f)
-       # _second_line()
+        _good_file(path)
+        _first_line(f)
+        _second_line()
 
 
 if __name__ == "__main__":
