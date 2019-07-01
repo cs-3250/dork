e25260941e516e31a2f84d510835c04ba67f7d8d
Casey Jones <cjone183@msudenver.edu>
2019-07-01 01:06:49 -0600

    remove game & maze_generation modules; comments & fixes in game_engine
    GameEngine:
    	delete GameWorld() method, add world attribute
    	shorten long lines in maze_generation()
    	import only classes we use from numpy
    	comment out trailing if statement with no condition
    	add FIXME comments for movement() method

--- dork/game_engine.py
+++ dork/game_engine.py
@@ -1,11 +1,13 @@
 import mazelib
 import networkx as nx
-import numpy as np
+from numpy import ndarray, ndenumerate
 
+from dork.objects import Room
 
 class GameEngine:
 
-    def GameWold():
+    def __init__(self):
+        self.world = None
 
     def save(self):
         G = nx.write_yaml('dork_save.dp')
@@ -15,52 +17,69 @@ class GameEngine:
 
     def load(self):
         print('Loading Game...\n')
-        
-        if:
-            
-        else: 
-            print('There is no game saved!')
-    
+
+        #if: # FIXME commented out for now, because syntax error. - Casey
+
+        #else:
+            #print('There is no game saved!')
+
     def reset_game(self):
         # Objects in game set to start
         pass
 
     def movement(self, direction):
-        # 
         #
         #
         #
-        if direction not in self.GameWorld:
-          print('Cannot move in that direction!')  
-          return
+        #
+        if direction not in self.world:
+            # FIXME self.world contains Rooms - not directions
+            # it's the edges of the individual nodes that have edge attributes
+            # you need to check the edges coming out from the current location
+            # see: https://stackoverflow.com/a/33252014/837710
+            # Please delete my comments once you fix this stuff. ;)
+            # - Casey
+            print('Cannot move in that direction!')
+            return
         new_room_name = self.world['room']
+        # FIXME this is close, but our graph is indexed by objects - not strings
+        # so you could look up a node like self.world[RoomObject]
+        # but the problem is that we don't have a reference to that Room yet
+        # you need to find the right edge, and that will point to it
+        # - Casey
         print('Moving to ', new_room_name)
         self.room = world[new_room_name]
+        # this should work once you fix line 42
+        # - Casey
 
 
-    def maze_generation(self):
-       MAZE_BIGNESS = (3, 3)
+    def maze_generation(self, size=(3, 3)):
 
-        class Room():
-        pass
-
-        maze = mazelib.Prims(*MAZE_BIGNESS).generate()
+        maze = mazelib.Prims(*size).generate()
         G = nx.DiGraph()
 
-        rooms = np.ndarray(shape=[d - 2 for d in maze.shape], dtype=Room)
+        self.world = ndarray(shape=[d - 2 for d in maze.shape], dtype=Room)
 
-        for index, room in np.ndenumerate(rooms):
+        for index, room in ndenumerate(self.world):
             x = index[0]
             y = index[1]
             x_maze = x + 1
             y_maze = y + 1
             if maze[x_maze, y_maze] == 0:
-                rooms[index] = Room()
+                self.world[index] = Room()
                 x_north = x - 1
-                if x_north >= 0 and rooms[(x_north, y)] is not None:
-                    G.add_edge(rooms[index], rooms[(x_north, y)], direction='n')
-                    G.add_edge(rooms[(x_north, y)], rooms[index], direction='s')
+                if x_north >= 0 and self.world[(x_north, y)] is not None:
+                    G.add_edge(self.world[index],
+                               self.world[(x_north, y)],
+                               direction='n')
+                    G.add_edge(self.world[(x_north, y)],
+                               self.world[index],
+                               direction='s')
                 y_west = y - 1
-                if y_west >= 0 and rooms[(x, y_west)] is not None:
-                    G.add_edge(rooms[index], rooms[(x, y_west)], direction='w')
-                    G.add_edge(rooms[(x, y_west)], rooms[index], direction='e')
+                if y_west >= 0 and self.world[(x, y_west)] is not None:
+                    G.add_edge(self.world[index],
+                               self.world[(x, y_west)],
+                               direction='w')
+                    G.add_edge(self.world[(x, y_west)],
+                               self.world[index],
+                               direction='e')
