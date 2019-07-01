b52c4c8346587ac4bfa775e836e61763adb8a524
Greg Dews <gdews@msudenver.edu>
2019-07-01 14:47:49 -0600

    dictionary exists tests...

---
+++ tests/test_dork_gamedictionary.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''basic tests for the dork cli'''
+from types import FunctionType
+from dork import gamedictionary
+from tests.utils import is_a
+
+
+def test_dictionaries_exist():
+    '''The dork module should define an Player.'''
+    assert "ACTION" in vars(gamedictionary)
+    is_a(gamedictionary.ACTION, dict)
+    is_a(gamedictionary.MOVEMENT, dict)
+    is_a(gamedictionary.PICK, dict)
