944d7e7116b4cb07f5fbe755a975e224f65a06c3
Daniel LEpiscopo <dannyjlepiscopo@gmail.com>
2019-07-01 13:15:58 -0600

    Updated the requirements-dev.txt, and the requirements.txt files

--- dork/game_engine.py
+++ dork/game_engine.py
@@ -1,33 +1,40 @@
-import networkx as mx
-from numpy import ndarray, ndenumerate
+from random import choice
+import networkx as nx
+from numpy import ndarray, ndenumerate as np
 from dork.objects import Room
 
 __all__ = ["save", "load", "movement", "maze_generation"]
 
-G = nx.Graph()
-
 def __init__(self):
     self.world = None
 
-def save(file_name='dork_save.dp'):
+def save(self, file_name='dork_save.dp'):
     """ Save game to file """
     print('Saving Game...')
-    G = nx.write_yaml(G, file_name)
+    self.world = nx.write_yaml(self.world, file_name)
     print('Game progress has been saved.')
 
-def load(file_name='dork_save.dp'):
+def load(self, file_name='dork_save.dp'):
     """ Load game from file """
     print('Loading Game...\n')
     try:
         with open(file_name):
-            G = nx.read_yaml(file_name)
+            self.world = nx.read_yaml(file_name)
     except IOError:
         return "There was a problem loading your game."
-    return G
 
-def movement(self):
-    pass
-    # location = self.player_location
+def movement(self, direction):
+    self.player_location
+    # check if the direction is in a tuplue with all of our directions
+    if direction in ('n', 'e', 's', 'w'):
+        pass
+    else:
+        return "you can't go that way!"
+
+    self.world.add_nodes_from()
+    self.world.add_edge()
+    self.world.add_edge()
+
     # GameEngine.movement(w)
     # Returns a string
 
@@ -60,21 +67,22 @@ def maze_generation(self):
             ROOMS[index] = ROOMS()
             x_north = x - 1
             if x_north >= 0 and self.world[(x_north, y)] is not None:
-                    G.add_edge(self.world[index],
+                    self.world.add_edge(self.world[index],
                                self.world[(x_north, y)],
                                direction='n')
-                    G.add_edge(self.world[(x_north, y)],
+                    self.world.add_edge(self.world[(x_north, y)],
                                self.world[index],
                                direction='s')
             y_west = y - 1
             if y_west >= 0 and self.world[(x, y_west)] is not None:
-                    G.add_edge(self.world[index],
+                    self.world.add_edge(self.world[index],
                                self.world[(x, y_west)],
                                direction='w')
-                    G.add_edge(self.world[(x, y_west)],
+                    self.world.add_edge(self.world[(x, y_west)],
                                self.world[index],
                                direction='e')
-    self.player_location
+
+    self.player_location = choice(list(self.world.nodes))
 
 if __name__ == "__main__":
     pass
