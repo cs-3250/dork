13597a4f5bd40d780417dd03a09dd57b19719b65
Casey Jones <cjone183@msudenver.edu>
2019-07-01 23:16:10 -0600

    add test for GameEngine.maze_generation()

--- tests/test_dork_game_engine.py
+++ tests/test_dork_game_engine.py
@@ -1,7 +1,9 @@
 """ Tests for the game engine """
 
 # from tests.utils import is_a
+from networkx import DiGraph
 from dork.game_engine import GameEngine
+from dork.objects import Room
 
 
 def test_save():
@@ -13,6 +15,7 @@ def test_save():
     assert "0" in out, \
         "Game_Engine.save method couldn't save game data"
 
+
 def test_load():
     ''' Loads in data to parse '''
     game = GameEngine()
@@ -20,6 +23,22 @@ def test_load():
     out = game.load("Trash")
     assert "Try again" in out, \
         "Game_Engine.load method couldn't find load data"
+
+
+def test_maze_generation():
+    '''maze_generation should:
+         populate world with nonempty graph of Room nodes
+         populate player_location with a Room'''
+    game_engine = GameEngine()
+    game_engine.world = None
+    game_engine.player_location = None
+    game_engine.maze_generation()
+    assert isinstance(game_engine.world, DiGraph)
+    assert isinstance(game_engine.world, Room)
+    assert game_engine.world  # sequence nonempty
+    for room in game_engine.world:
+        assert isinstance(room, Room)
+
 
 # def test_movement():
 #     ''' Testing player movement if its  '''
