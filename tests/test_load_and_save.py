dca0e269f33f75af978492467b03f52e4ec81f43
Derek Haviland <dhavila2@msudenver.edu>
2019-07-13 15:54:42 -0600

    Started TDD for load and save

---
+++ tests/test_load_and_save.py
@@ -0,0 +1,20 @@
+"""Tests save and load"""
+
+import os
+import yaml
+
+
+def check_filepath(file_path):
+    """Checks for a valid filepath"""
+    # assert for a file path check? If it is correct return the path, if not throw an error message
+    return os.path.isfile(file_path)
+
+def test_load():
+    """Load should load previous player gamestate"""
+    #Add asserts for testing load
+    data = dork.load_and_save.load("")
+    return data
+
+def test_save():
+    """Save should write current gamestate to a file"""
+    #Continue developing later
