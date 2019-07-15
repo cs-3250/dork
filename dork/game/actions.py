4c8d6ebc443e10b5c1bfb0a32376224061ed1d35
Greg Dews <gdews@msudenver.edu>
2019-07-15 09:39:31 -0600

    Added stubs to actions

--- dork/game/actions.py
+++ dork/game/actions.py
@@ -2,30 +2,24 @@
 """THE GAME DICTIONARY
 """
 
-__all__ = ['ACTION_CHOICES', 'PICK', 'MOVEMENT', '_danger_will_robinson',
-           '_move']
+__all__ = ['ACTION_CHOICES', '_cry', '_danger_will_robinson',
+           '_jump', '_move', '_pick', '_run']
 
-PICK = {'up': "nothing picked up you have nowhere to put it"
-        }
 
-MOVEMENT = {'north': 'You have moved north',
-            'n': 'You have moved north',
-            'east': 'You have moved east',
-            'e': 'You have moved east',
-            'south': 'You have moved south',
-            's': 'You have moved south',
-            'west': 'You have moved west',
-            'w': 'You have moved west'
-            }
-
-ACTION_CHOICES = {'jump': '_jump',
-                  'run': '_danger_will_robinson',
-                  'cry': '_danger_will_robinson',
+ACTION_CHOICES = {'cry': '_cry',
                   'go': '_move',
-                  'load': '_load',
-                  'save': '_save',
-                  'pick': PICK,
+                  'jump': '_jump',
+                  'load': '_danger_will_robinson',
+                  'pick': '_danger_will_robinson',
+                  'run': '_run',
+                  'save': '_danger_will_robinson',
                   }
+
+
+def _cry(self, *args):
+    """crying action stub"""
+    response = 'After curling into a ball you cried. Poor you.'
+    return response
 
 
 def _danger_will_robinson(engine, *args):
@@ -34,17 +28,33 @@ def _danger_will_robinson(engine, *args):
 
 def _jump(self, *args):
     """jumping action stub"""
-    return 'you have jumped' + args
+    response = 'You have jumped, just not sure why.'
+    return response
 
 
-def _move(direction):
+def _move(self, direction):
     """ Player movement """
     directions = {'n': 'north',
                   's': 'south',
                   'w': 'west',
-                  'e': 'east'}
-
+                  'e': 'east',
+                  'north': 'north',
+                  'south': 'south',
+                  'west': 'west',
+                  'east': 'east',
+                  }
     if direction not in directions:
-        return "Invalid direction! Type a different command."
-
+        return "Sorry, you can't go that direction. Type a different command."
     return "You moved " + direction
+
+
+def _pick(self, *args):
+    """pick up action stub"""
+    response = 'You picked up ' + args
+    return response
+
+
+def _run(self, *args):
+    """running action stub"""
+    response = 'You ran in place. Are you just bored?'
+    return response
