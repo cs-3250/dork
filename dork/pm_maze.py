5dba4671d5c1ec7ac020dae0a66ddc8ac4021121
EdwinHernandez21 <eherna62@msudenver.edu>
2019-07-17 13:24:56 -0600

    error fixed

--- dork/pm_maze.py
+++ dork/pm_maze.py
@@ -8,7 +8,7 @@ from dork.objects import Player, Room, Holder
 class Maze:
     """maze class"""
 
-     def __init__(self, file_name="dork/ypm_maze.yml"):
+    def __init__(self, file_name="dork/ypm_maze.yml"):
         """Maze starting"""
         with open(file_name, "r") as file_descriptor:
             self.data = yaml.safe_load(file_descriptor)
