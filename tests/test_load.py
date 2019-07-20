6bd5c694e18d8e299492c6c3372266cb2fdd2593
Daniel LEpiscopo <dannyjlepiscopo@gmail.com>
2019-07-20 14:08:22 -0600

    Deleting files that we don't need

--- tests/test_load.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Load in a yaml file test """
-
-from dork.load import load
-
-
-def test_load():
-    """ Testing the load method """
-    assert isinstance(load.load(), dict)
